4c0fcf3c57e4fc1b448c8cdfd0f8ed9c70117f23
samvrny <samvrny@gmail.com>
2022-11-29 18:16:53 -0600

    got the company names to print to a select list

--- public/assets/javascript/populate-lists.js
+++ public/assets/javascript/populate-lists.js
@@ -1,7 +1,32 @@
+//grabbing the select lists for guests, companies, and greetings by their ids
 const guestSelect = document.getElementById('guest-list')
+const companySelect = document.getElementById('company-list');
 
+//fetching the guest list to populate the guest select list
 function populateGuests() {
     fetch('/guests', {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if(response.ok) {
+            //possibly print call to printGuests here.
+            return response.json();
+        } else {
+            //display error onscreen
+        }
+    })
+    .then(response => {
+        printGuests(response)
+    })
+}
+
+//fetching the company list to populate the company select list
+function populateCompanies() {
+    fetch('/company', {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -16,16 +41,27 @@ function populateGuests() {
         }
     })
     .then(response => {
-        printGuests(response)
+        printCompanies(response)
     })
 }
 
+//printing the guest list in the guest select option
 function printGuests(guests) {
     for(i=0; i < guests.length; i++) {
         const guestOption = document.createElement('option');
         guestOption.textContent = guests[i].firstName + ' ' + guests[i].lastName;
-        guestSelect.appendChild(guestOption)
+        guestSelect.appendChild(guestOption);
+    }
+}
+
+//printing the company list in the company select option
+function printCompanies(company) {
+    for(i=0; i < company.length; i++) {
+        const companyOption = document.createElement('option');
+        companyOption.textContent = company[i].company;
+        companySelect.appendChild(companyOption);
     }
 }
 
 populateGuests();
+populateCompanies();
